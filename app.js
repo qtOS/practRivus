c9ae09c413587ec2e7d1b6d7b8e37061d90e8b4c
Aaron <akr.git.receiver@gmail.com>
2016-02-09 18:30:08 -0600

    changed 3000 to port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
 		app = express(),
-		server = require('http').Server(app).listen(3000),
+		server = require('http').Server(app).listen(port),
  		io = require('socket.io')(server),
 		mongoose = require('mongoose'),
 		path = require('path'),
